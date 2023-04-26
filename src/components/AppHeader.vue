42347e23ae0e9fdbd2d468f36818b0c099c91637
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-26 17:50:39 +0200

    Add: main-navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,18 @@
 <script>
+import AppLogo from "./AppLogo.vue";
 export default {
   name: "AppHeader",
+  components: {
+    AppLogo
+  }
 };
 </script>
 
 <template>
   <header>
+    <!-- CONTACT NAVBAR -->
     <div class="contact-navbar p-1">
-      <div class="ms-container d-flex justify-content-between">
+      <div class="ms-container d-flex justify-content-between pt-2 pb-2">
         <p><i class="fa-solid fa-clock pe-2"></i>Open Hours: Mon - Sat - <time>9:00</time> - <time>18:00</time></p>
         <ul class="d-flex">
             <li><i class="fa-solid fa-phone pe-2"></i>+1 (305) 1234-5678</li>
@@ -18,21 +23,57 @@ export default {
         </ul>
       </div>
     </div>
+    <!-- /CONTACT NAVBAR -->
+    
+    <!-- MAIN NAVBAR -->
+    <div class="ms-container">
+        <div class="main-navbar d-flex justify-content-between align-items-center text-uppercase pt-4">
+            <AppLogo />
+            <ul class="d-flex ">
+                <li>
+                    <a href="#">home</a>
+                </li>
+                <li>
+                    <a href="#">services</a>
+                </li>
+                <li>
+                    <a href="#">skills</a>
+                </li>
+                <li>
+                    <a href="#">partners</a>
+                </li>
+                <li>
+                    <a href="#">blog</a>
+                </li>
+                <li>
+                    <button class="primary-btn btn">get in touch</button>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- MAIN NAVBAR -->
+    
+
   </header>
 </template>
 
 <style scoped lang="scss">
 @use "../styles/partials/variables" as *;
+@use "../styles/general.scss";
 
-.contact-navbar {
-  background-color: $contact-navbar-color;
-  color: white;
-  ul, p {
-    margin: unset;
-    padding: .5rem 0;
-    li{
-        margin-left: 2rem;
+header {
+    color: white;
+    ul, p {
+        margin: unset;
+        li{
+            margin-left: 2rem;
+            cursor: pointer;
+        }
+
     }
-  }
+    .contact-navbar {
+      background-color: $contact-navbar-color;
+    }
 }
 </style>
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "AppLogo",
+};
+</script>
+
+<template>
+  <p class="fs-4 text-uppercase"><span>nex</span> gen</p>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/partials/variables" as *;
+p {
+  font-weight: bold;
+  letter-spacing: 5px;
+}
+span {
+  display: inline-block;
+  background-color: rgba($color: $primary-color, $alpha: 0.7);
+  color: $primary-color;
+  line-height: 50px;
+  width: 100px;
+  text-align: right;
+  border-top-left-radius: 50px;
+  border-bottom-left-radius: 50px;
+}
+</style>
