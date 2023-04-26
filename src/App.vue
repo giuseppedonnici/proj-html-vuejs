8b07490075dd791315313f671a996195e0ea70d5
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-26 15:47:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader
+  }
+};
+</script>
+
+<template>
+  
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <div class="contact-navbar p-1">
+      <div class="container">
+        <p><i class="fa-solid fa-clock pe-2"></i>Open Hours: Mon - Sat - <time>9:00</time> - <time>18:00</time></p>
+        <ul>
+            <li><i class="fa-solid fa-phone pe-2"></i>+1 (305) 1234-5678</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/partials/variables" as *;
+
+.contact-navbar {
+  background-color: $contact-navbar-color;
+  color: white;
+}
+</style>
