2661d69466a0c466811c514420c0aa48c110d3d4
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-26 16:06:45 +0200

    Add: contact-navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,14 +7,14 @@ export default {
 <template>
   <header>
     <div class="contact-navbar p-1">
-      <div class="container">
+      <div class="ms-container d-flex justify-content-between">
         <p><i class="fa-solid fa-clock pe-2"></i>Open Hours: Mon - Sat - <time>9:00</time> - <time>18:00</time></p>
-        <ul>
+        <ul class="d-flex">
             <li><i class="fa-solid fa-phone pe-2"></i>+1 (305) 1234-5678</li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><i class="fa-solid fa-envelope pe-2"></i>hello@example.com</li>
+            <li><i class="fa-brands fa-facebook-f"></i></li>
+            <li><i class="fa-brands fa-twitter"></i></li>
+            <li><i class="fa-brands fa-linkedin-in"></i></li>
         </ul>
       </div>
     </div>
@@ -27,5 +27,12 @@ export default {
 .contact-navbar {
   background-color: $contact-navbar-color;
   color: white;
+  ul, p {
+    margin: unset;
+    padding: .5rem 0;
+    li{
+        margin-left: 2rem;
+    }
+  }
 }
 </style>
