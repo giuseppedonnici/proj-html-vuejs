c40d4ac0166c95c5066bc12e3d70fc60419c8001
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-27 18:41:15 +0200

    Add: NewsCard

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: "NewsCard",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<template>
+  <div class="card">
+    <h5 class="fw-bold text-center p-4">
+      {{ title }}
+    </h5>
+    <div class="card-body rounded-4"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  position: relative;
+  height: 100%;
+  border: unset;
+  h5 {
+    position: absolute;
+    bottom: 0;
+    z-index: 1;
+    color: white;
+  }
+  .card-body {
+    background-image: url(../assets/img/news-1.jpg);
+    background-size: cover;
+    filter: brightness(0.5);
+  }
+}
+</style>
--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -1,19 +1,44 @@
 <script>
+import NewsCard from './NewsCard.vue';
+
 export default {
   name: "NewsSections",
-};
+  components: {
+    NewsCard
+  }
+
+}
 </script>
 
 <template>
   <section id="blog">
     <div class="ms-container">
-      <div class="news-description">
+
+      <div class="news-description mb-5">
         <h6>our editorial content</h6>
         <h2>Latest <span class="special-span">News</span></h2>
         <p>
           Every week we publish content about what is best in the business world.
         </p>
         <button class="btn primary-btn">see all</button>
+      </div>
+
+      <div class="row row-cols-lg-3 gx-3">
+          <div class="col">
+            <NewsCard 
+            title="Increasing creativity is possible for everyone"
+            />
+          </div>
+          <div class="col">
+            <NewsCard 
+            title="Because market research is part of the business plan"
+            />
+          </div>
+          <div class="col">
+            <NewsCard 
+            title="Working from home is now a trend"
+            />
+          </div>
       </div>
     </div>
   </section>
@@ -32,6 +57,10 @@ export default {
         bottom: 0;
         right: 0;
     }
+}
+
+.col {
+    aspect-ratio: 1 / 1;
 }
 
 </style>
